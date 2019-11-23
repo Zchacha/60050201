2a267a15fb91e09f7448076200b87be7bf2642e7
chacha <60050201@kmitl.ac.th>
2019-11-23 16:57:11 +0700

    init project

---
+++ src/th/co/cdgs/bean/CustomerDto.java
@@ -0,0 +1,56 @@
+package th.co.cdgs.bean;
+
+public class CustomerDto {
+	
+	private Long customerId;
+	private String fullName;
+	private String address;
+	private String tel;
+	private String email;
+	private String firstname;
+	private String lastname;
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFistname(String fistname) {
+		this.firstname = fistname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
